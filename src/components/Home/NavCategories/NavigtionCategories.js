8d42b91e080d7dce5d9646d6953ed2ae6de4f8c9
NiKiTo599 <akulichnikita0@gmail.com>
2020-03-31 18:55:54 +0300

    feat: make navigate categories for mobile

--- src/components/Home/NavCategories/NavigtionCategories.js
+++ src/components/Home/NavCategories/NavigtionCategories.js
@@ -9,6 +9,7 @@ import { saveCategories } from "./../../../actions";
 import { graphql } from "react-apollo";
 import { compose } from "recompose";
 import { categoriesQuery } from "./queries";
+import HideMenu from "../../basicComponents/HideMenu";
 
 class NavigtionCategories extends React.Component {
   shouldComponentUpdate = () => {
@@ -33,54 +34,79 @@ class NavigtionCategories extends React.Component {
 
   handleClick = e => {
     const { currentTarget } = e;
-    currentTarget.children[2].classList.toggle("displayNone");
+    currentTarget.children[2].classList.toggle("maxHeight");
   };
 
+  /*showCategories = () => {
+    const categories = document.querySelector('.container-categories');
+    categories.classList.toggle('displayFlex')
+  }*/
+
   render() {
+    const { width } = this.props;
     if (this.props.categories) {
       const keys = Object.keys(this.props.categories);
-      return keys.length > 1 ? (
-        <ul className="categories-list">
-          {keys.map((item, idx) => (
-            <>
-              {this.props.categories[item].length !== 1 ? (
-                <li
-                  key={idx * Math.random() * 100}
-                  className="list__main-item"
-                  onClick={this.handleClick}
-                >
-                  <span>{item}</span>
-                  <div className="container-for-svg">
-                    <FontAwesomeIcon icon={faAngleDown} />
-                  </div>
-                  <ul className="sub-list" onClick={e => e.stopPropagation()}>
-                    {this.props.categories[item].map((subItem, index) => (
-                      <li key={index * Math.random() * 100} className="sub-list__item">
-                        <Link
-                          className="nav__link"
-                          key={index * Math.random() * 100}
-                          to={`/home?query=${subItem._id}&page=1&selection=false`}
-                        >
-                          <FontAwesomeIcon icon={faChevronRight} />
-                          {subItem.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ) : (
-                <li key={idx * Math.random() * 100} className="list__main-item">
-                  <Link
-                    className="nav__link"
-                    to={`/home?query=${this.props.categories[item][0]._id}&page=1&selection=false`}
+      const navCatComponent = (
+        <aside className="container-categories">
+          <ul className="categories-list">
+            {keys.map((item, idx) => (
+              <>
+                {this.props.categories[item].length !== 1 ? (
+                  <li
+                    key={idx * Math.random() * 100}
+                    className="list__main-item"
+                    onClick={this.handleClick}
                   >
                     <span>{item}</span>
-                  </Link>
-                </li>
-              )}
-            </>
-          ))}
-        </ul>
+                    <div className="container-for-svg">
+                      <FontAwesomeIcon icon={faAngleDown} />
+                    </div>
+                    <ul className="sub-list" onClick={e => e.stopPropagation()}>
+                      {this.props.categories[item].map((subItem, index) => (
+                        <li
+                          key={index * Math.random() * 100}
+                          className="sub-list__item"
+                        >
+                          <Link
+                            className="nav__link"
+                            key={index * Math.random() * 100}
+                            to={`/home?query=${subItem._id}&page=1&selection=false`}
+                          >
+                            <FontAwesomeIcon icon={faChevronRight} />
+                            {subItem.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ) : (
+                  <li
+                    key={idx * Math.random() * 100}
+                    className="list__main-item"
+                  >
+                    <Link
+                      className="nav__link"
+                      to={`/home?query=${this.props.categories[item][0]._id}&page=1&selection=false`}
+                    >
+                      <span>{item}</span>
+                    </Link>
+                  </li>
+                )}
+              </>
+            ))}
+          </ul>
+        </aside>
+      );
+      return keys.length > 1 ? (
+        <>
+          {width <= 768 ? (
+            <HideMenu title="Категории" hiddenClass="hide_nav_categories">
+              {navCatComponent}
+            </HideMenu>
+          ) : (
+            navCatComponent
+          )}
+        </>
       ) : null;
     } else {
       return null;
